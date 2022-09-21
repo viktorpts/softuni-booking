2daa63010a1e8c35a8581affb43e75909185bbd8
Viktor Kostadinov <viktor.pts@abv.bg>
2022-09-21 20:23:33 +0300

    Room creation page

--- controllers/createController.js
+++ controllers/createController.js
@@ -1,3 +1,5 @@
+const { create } = require('../services/accomodationService');
+
 const router = require('express').Router();
 
 
@@ -5,6 +7,17 @@ router.get('/', (req, res) => {
     res.render('create', {
         title: 'Host New Accomodation'
     });
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const result = await create(req.body);
+        res.redirect('/catalog/' + result.id);
+    } catch(err) {
+        res.render('create', {
+            title: 'Request Error'
+        });
+    }
 });
 
 module.exports = router;
--- services/accomodationService.js
+++ services/accomodationService.js
@@ -23,7 +23,29 @@ function getById(id) {
     return data.find(i => i.id == id);
 }
 
+async function create(roomData) {
+    const room = {
+        id: getId(),
+        name: roomData.name,
+        description: roomData.description,
+        city: roomData.city,
+        beds: Number(roomData.beds),
+        price: Number(roomData.price),
+        imgUrl: roomData.imgUrl
+    };
+
+    data.push(room);
+    await persist();
+
+    return room;
+}
+
+function getId() {
+    return ('000000' + (Math.random() * 999999 | 0).toString(16)).slice(-6);
+}
+
 module.exports = {
     getAll,
-    getById
+    getById,
+    create
 };
