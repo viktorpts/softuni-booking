ef0ee08d421c31ea826af6838e3fa9b80b36dbbd
Viktor Kostadinov <viktor.pts@abv.bg>
2022-09-28 21:44:57 +0300

    Add Facility editor

--- controllers/facilityController.js
+++ controllers/facilityController.js
@@ -26,6 +26,11 @@ facilityController.get('/:roomId/decorateRoom', async (req, res) => {
     const roomId = req.params.roomId;
     const room = await getById(roomId);
     const facilities = await getAllFacilities();
+    facilities.forEach(f => {
+        if ((room.facilities || []).some(id => id.toString() == f._id.toString())) {
+            f.checked = true;
+        }
+    });
 
     res.render('decorate', {
         title: 'Add Facility',
--- services/facilityService.js
+++ services/facilityService.js
@@ -1,4 +1,5 @@
 const Facility = require('../models/Facility');
+const Room = require('../models/Room');
 
 
 async function getAllFacilities() {
@@ -13,7 +14,32 @@ async function createFacility(label, iconUrl) {
 }
 
 async function addFacilities(roomId, facilityIds) {
+    const room = await Room.findById(roomId).populate('facilities');
+    const facilities = await Facility.find({ _id: { $in: facilityIds } });
 
+    // Remove room ref from removed facilities
+    const toRemove = room.facilities.filter(f => facilities.every(x => x._id.toString() != f._id.toString()));
+    console.log('To remove', toRemove.map(x => x.label));
+    toRemove.forEach(f => {
+        // Remove Room from Facility
+        f.rooms.splice(f.rooms.findIndex(rId => rId.toString() == roomId), 1);
+        // Remove Facility from Room
+        room.facilities.splice(room.facilities.findIndex(x => x._id.toString() == f._id.toString()), 1);
+    });
+
+    // Determine new facilities
+    const newlyAdded = facilities.filter(f => room.facilities.every(x => x._id.toString() != f._id.toString()));
+    console.log('New', newlyAdded.map(x => x.label));
+
+    // Add room ref to newly added facilities
+    newlyAdded.forEach(f => {
+        room.facilities.push(f);
+        f.rooms.push(room);
+    });
+
+    await room.save();
+    await Promise.all(toRemove.map(f => f.save()));
+    await Promise.all(newlyAdded.map(f => f.save()));
 }
 
 module.exports = {
