90ba820996b0e8b0d273fa9dd38723339c287f49
Viktor Kostadinov <viktor.pts@abv.bg>
2022-10-10 20:50:32 +0300

    Add validator libs; add error parser

---
+++ utils/parser.js
@@ -0,0 +1,30 @@
+function parseError(error) {
+    // check type of error
+    // if Array -> express validator, take msg and param props from array
+    // else if error.name = ValidationError -> Mongoose validation, take error.entries => ([field, e]) => [field, e.message]
+    // else, process regular error, take message prop
+    // return { messages: [String], fields: Object }
+
+    const result = {
+        messages: [],
+        fields: {}
+    };
+
+    if (error.name == 'ValidationError') {
+        for (let [field, e] of Object.entries(error.errors)) {
+            result.messages.push(e.message);
+            result.fields[field] = field;
+        }
+    } else if (Array.isArray(error)) {
+        result.messages = error.map(e => e.msg);
+        result.fields = Object.fromEntries(error.map(e => [e.param, e.param]));
+    } else {
+        result.messages.push(error.message);
+    }
+
+    return result;
+}
+
+module.exports = {
+    parseError
+};
